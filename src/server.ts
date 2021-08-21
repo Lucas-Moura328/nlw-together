578737f1d253a7c00f0e611c6ad59e9a3eb8d314
Lucas-Moura328 <lucasjosemoura649@gmail.com>
2021-08-21 15:28:49 -0300

    Dia 02
    Migrations, Controllers, ORM

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,14 @@
+import "reflect-metadata";
 import express from 'express';
-import { response } from 'express';
-import { request } from 'express';
 
-//@types/express
+import { router } from "./routes";
+
+import "./database";
+
 const app = express();
 
-/*
-*GET    => BUSCAR INFORMAÇÃO
-*POST   => INSERIR INFORMAÇÃO
-*PUT    => ALTERAR INFORMAÇÃO
-*DELETE => DELETAR DADO
-*PATCH  => ALTERAR INFORMAÇÃO ESPECIFICA
-*/
+app.use(express.json( ))
 
-// PASSAR RECURSO = ROTA
-app.get("/teste", (request, response) => {
-    //Request => entrando
-    //Response => Saindo
-    return response.send("Aoba");
-})
+app.use(router);
 
-app.post("/teste-post", (request, response) => {
-    return response.send("Aoba post")
-});
-
-//http://localhost:3000
 app.listen(3000, () => console.log("server rodando"));
