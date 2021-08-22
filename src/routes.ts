2e10603f4ca368eac0d6c46964c12b82cc2218a0
Lucas-Moura328 <lucasjosemoura649@gmail.com>
2021-08-22 04:33:48 -0300

    Dia 04
    Criptografar com bcrypt, criar tabelas com relacionamentos

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,19 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentControlller";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createtagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createtagController.handle);
-
+router.post("/login", authenticateUserController.handle);
+router.post("/compliments", createComplimentController.handle);
 
 export { router };
