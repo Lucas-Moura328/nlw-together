e17c477eeec4377be4a88187119c7800b4273e80
Lucas-Moura328 <lucasjosemoura649@gmail.com>
2021-08-23 18:17:34 -0300

    Dia 05-1

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,10 @@ import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentControlller";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListTagsController } from "./controllers/ListTagController";
 
 const router = Router();
 
@@ -11,10 +15,18 @@ const createUserController = new CreateUserController();
 const createtagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController = new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
+
+const listTagsController = new ListTagsController();
 
 router.post("/users", createUserController.handle);
-router.post("/tags", ensureAdmin, createtagController.handle);
+router.post("/tags", ensureAuthenticated, ensureAdmin, createtagController.handle);
 router.post("/login", authenticateUserController.handle);
-router.post("/compliments", createComplimentController.handle);
+router.post("/compliments", ensureAuthenticated,createComplimentController.handle);
 
+router.get("/tags", ensureAuthenticated, listTagsController.handle);
+
+router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle)
+router.get("/users/compliments/receive", ensureAuthenticated,  listUserReceiveComplimentsController.handle)
 export { router };
